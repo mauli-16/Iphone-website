f0a32e8c494b27f4b2a4224e30a50d8852b278bb
Mauli Saxena <maulisaxena.16@gmail.com>
2025-02-02 12:33:14 +0530

    added light and view

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
 import { Html, OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
 
-const ModelView = () => {
+import * as THREE from 'three'
+import Lights from './Lights';
+import Loader from './Loader';
+import IPhone from './IPhone';
+import { Suspense } from "react";
+
+const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, size, item }) => {
   return (
-    <div>ModelView</div>
+    <View
+      index={index}
+      id={gsapType}
+      className={`w-full h-full absolute ${index === 2 ? 'right-[-100%]' : ''}`}
+    >
+      {/* Ambient Light */}
+      <ambientLight intensity={0.3} />
+
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+      <Lights />
+
+
+      
+    </View>
   )
 }
 
