77590f4357a6834f6995f919eb326050dfe4e504
Mauli Saxena <maulisaxena.16@gmail.com>
2025-02-02 13:42:49 +0530

    made Iphone.jsx(mesh) and added orbital control

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -2,7 +2,7 @@ import { Html, OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
 
 import * as THREE from 'three'
 import Lights from './Lights';
-import Loader from './Loader';
+//import Loader from './Loader';
 import IPhone from './IPhone';
 import { Suspense } from "react";
 
@@ -19,6 +19,25 @@ const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, si
       <PerspectiveCamera makeDefault position={[0, 0, 4]} />
 
       <Lights />
+
+      <OrbitControls 
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0 ,0)}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+      /> 
+      <group ref={groupRef} name={`${index === 1} ? 'small' : 'large`} position={[0, 0 ,0]}>
+        <Suspense >
+          <IPhone 
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            item={item}
+            size={size}
+          />
+        </Suspense>
+      </group>
 
 
       
